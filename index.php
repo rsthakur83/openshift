13141993d4571afa8142ab7e48b249d6b4e9181c
rsthakur83 <rsthakur83@yahoo.com>
2017-05-18 02:25:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
-<?php
-        //Enter your code here, enjoy!
+<html>
+	<head>
+	<title>My first PHP page</title>
+	</head>
+	<body>
 
-$array = array("1" => "PHP code tester Sandbox Online",  
-              "foo" => "bar", 5 , 5 => 89009, 
-              "case" => "Random Stuff: " . rand(100,999),
-              "PHP Version" => phpversion()
-              );
-              
-foreach( $array as $key => $value ){
-    echo $key."\t=>\t".$value."\n";
-}
+	<?php   
+
+	echo "<h1>Hello World!</h1>";
+
+	?>
+
+	</body>
+	</html>
