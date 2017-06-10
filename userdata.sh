16f763835b48aab7100be661779ca6ebc1dd04a0
rsthakur83 <rsthakur83@yahoo.com>
2017-06-10 12:54:18 -0400

    release commit

--- userdata.sh
+++ userdata.sh
@@ -2,11 +2,6 @@
 yum -y install httpd wget git
 systemctl start httpd
 chmod 777 -R /var/www/html
-cd /var/www/html
-echo "<html lang="en"><head>" > index.html
-echo "<title>Example page</title>" >> index.html
-echo "<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">" >> index.html
-echo "</head>" >> index.html
-echo "<body>" >> index.html
-echo "<p><font size="100" color="green"><h1>            GREEN       </h1></p>" >> index.html
-echo "</body></html>" >> index.html
+git init
+git clone --depth 1 --branch release-0.05 git://github.com/rsthakur83/release.git
+mv release/* /var/www/html
