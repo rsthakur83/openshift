c8f7455ae7e98ac08790c70bcce7c84fb8a6615e
rsthakur83 <rsthakur83@yahoo.com>
2017-06-11 00:53:13 +0700

    Create userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,5 +3,3 @@ yum -y install httpd wget git
 systemctl start httpd
 chmod 777 -R /var/www/html
 git init
-git clone --depth 1 --branch release-0.05 git://github.com/rsthakur83/release.git
-mv release/* /var/www/html
