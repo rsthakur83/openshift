8fcc826b000e6d53c0eb98833de0e7bca0a18cf1
rsthakur83 <rsthakur83@yahoo.com>
2017-06-11 21:41:39 +0700

    Create scaling.sh

--- scaling.sh
+++ scaling.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-asg1=`aws autoscaling describe-auto-scaling-groups|grep AutoScalingGroupName|awk '{print $2}'|cut -c 2-19`
+asg1=`aws autoscaling describe-auto-scaling-groups --region us-east-1|grep AutoScalingGroupName|awk '{print $2}'|cut -c 2-19`
 echo "ASG 2 $asg1"
 lcfg1="machine-factory-v1"
 lcfg2="machine-factory-v2"
@@ -9,9 +9,9 @@ lcfg2="machine-factory-v2"
 if [ "$asg1" == "$lcfg1" ]
 then
   echo "Attach Green & Detach Blue Env"
-  aws autoscaling attach-load-balancers --auto-scaling-group-name $lcfg2 --load-balancer-names my-lb;sleep 60; aws autoscaling detach-load-balancers --auto-scaling-group-name $lcfg1 --load-balancer-names my-lb;echo "destroy";aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $lcfg1
+  aws autoscaling attach-load-balancers --auto-scaling-group-name  $lcfg2 --load-balancer-names my-lb --region us-east-1;sleep 60; aws autoscaling detach-load-balancers --auto-scaling-group-name $lcfg1 --load-balancer-names my-lb --region us-east-1;echo "destroy";aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $lcfg1 --region us-east-1
 else
   echo "Attach Green & Detach Blue Env"
-  aws autoscaling attach-load-balancers --auto-scaling-group-name $lcfg1 --load-balancer-names my-lb;sleep 60; aws autoscaling detach-load-balancers --auto-scaling-group-name $lcfg2 --load-balancer-names my-lb;echo "destroy";aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $lcfg2
+  aws autoscaling attach-load-balancers --auto-scaling-group-name $lcfg1 --load-balancer-names my-lb --region us-east-1;sleep 60; aws autoscaling detach-load-balancers --auto-scaling-group-name $lcfg2 --load-balancer-names my-lb --region us-east-1;echo "destroy";aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $lcfg2 --region us-east-1
 
 fi
