ce0080349a87f2d66f88a9a043494109a77ef65c
rsthakur83 <rsthakur83@yahoo.com>
2017-06-11 21:28:02 +0700

    Create green-webapp.tf

--- green-webapp.tf
+++ green-webapp.tf
@@ -81,7 +81,7 @@ resource "aws_security_group" "web_server1" {
 
 
 resource "aws_key_pair" "deployer" {
- key_name = "myapppkeypair2"
+ key_name = "myapppkeypair3"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDaj8nDzZrLXtfXpg3Uo71wrTfgVPMMLLdPRPPjj8hD1vRoAhkXpdluv4rDYJ6YucRZp7TGcnU2X6DaaEKMUayd7YAN99pf3v9Hm13n/dGc6rJZDTlu9jxaUuJIfSYQv4I7F/ZtixSsqYG1QAIw6KaRWhNO7OvfSMehMn0/4wVTS6fDJuCmo7MlFDU/KidRv3TT5sz+cc+OcYMUuczNtub3BSu+5x1sLLBQtYbntDs1oCAkJJ1yblE74dQMsNduC8fdFsRIJyRGGyQjCPB9gfImBlQu+zGgkgPmi2YEA1/x96v2jo6+L3ZIoFrpkmzuV8dGmmOrpEIEZZtKmi8DxP8L root@lap-am0044476.bccs.hutch.co.id"
 }
 
