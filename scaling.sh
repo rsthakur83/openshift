c71887d29835926e8ded1402d72309d62a4e7da1
rsthakur83 <rsthakur83@yahoo.com>
2017-06-12 19:18:16 +0700

    Create scaling.sh

--- scaling.sh
+++ scaling.sh
@@ -2,16 +2,28 @@
 
 
 asg1=`sudo aws autoscaling describe-auto-scaling-groups --region us-east-1|grep AutoScalingGroupName|awk '{print $2}'|cut -c 2-19`
+fis="fis"
+sec="sec"
+
 echo "ASG 2 $asg1"
 lcfg1="machine-factory-v1"
 lcfg2="machine-factory-v2"
 
 if [ "$asg1" == "$lcfg1" ]
 then
+  cd $fis;sudo terraform plan;sudo terraform apply
+  sleep 180
   echo "Attach Green & Detach Blue Env"
  sudo aws autoscaling attach-load-balancers --auto-scaling-group-name  $lcfg2 --load-balancer-names web-elb --region us-east-1
+  sleep 180
+ sudo aws autoscaling detach-load-balancers --auto-scaling-group-name $lcfg1 --load-balancer-names web-elb --region us-east-1
+ sudo aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $lcfg1 --region us-east-1 --force-delete
+
 else
+  cd $sec;sudo terraform plan;sudo terraform apply
+  sleep 180
   echo "Attach Green & Detach Blue Env"
  sudo aws autoscaling attach-load-balancers --auto-scaling-group-name $lcfg1 --load-balancer-names web-elb --region us-east-1
-
+  sleep 180
+ sudo aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $lcfg2 --region us-east-1 --force-delete
 fi
