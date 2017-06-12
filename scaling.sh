a2b687ea17fd5b162ac35234ab29660a942e6476
rsthakur83 <rsthakur83@yahoo.com>
2017-06-13 01:02:45 +0700

    Create scaling.sh

--- scaling.sh
+++ scaling.sh
@@ -11,7 +11,7 @@ lcfg2="machine-factory-v2"
 
 if [ "$asg1" == "$lcfg1" ]
 then
-  rm -rf $fis/*;cp userdata.sh $fis;cd $fis;sudo terraform plan;sudo terraform apply
+  rm -rf $fis/terraform.*;cp userdata.sh $fis;cd $fis;sudo terraform plan;sudo terraform apply
   sleep 180
   echo "Attach Green & Detach Blue Env"
  sudo aws autoscaling attach-load-balancers --auto-scaling-group-name  $lcfg2 --load-balancer-names web-elb --region us-east-1
