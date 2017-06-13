c029cbb8fa9d4bf37a2f8d7cfbeb74abfb405eb0
rsthakur83 <rsthakur83@yahoo.com>
2017-06-13 14:05:16 +0700

    Create scaling.sh

--- scaling.sh
+++ scaling.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-asg1=`sudo aws autoscaling describe-auto-scaling-groups --region us-east-1|grep AutoScalingGroupName|awk '{print $2}'|cut -c 2-19`
+#asg1=`sudo aws autoscaling describe-auto-scaling-groups --region us-east-1|grep AutoScalingGroupName|awk '{print $2}'|cut -c 2-19`
+asg1=`sudo aws autoscaling describe-launch-configurations --region us-east-1|grep LaunchConfigurationName|awk '{print $2}'|cut -c 2-19`
 fis="fis"
 sec="sec"
 sub1="subnet-de55faf2"
