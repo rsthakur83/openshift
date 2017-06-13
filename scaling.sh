a891a022fa6d9046713b4db31688a2ffc218f646
rsthakur83 <rsthakur83@yahoo.com>
2017-06-13 15:02:52 +0700

    Create scaling.sh

--- scaling.sh
+++ scaling.sh
@@ -15,14 +15,14 @@ lcfg2="machine-factory-v2"
 
 if [ "$asg1" == "$lcfg1" ]
 then
-  rm -rf $fis/terraform.*;cp userdata.sh $fis;cd $fis;sudo terraform plan;sudo terraform apply
+ # rm -rf $fis/terraform.*;cp userdata.sh $fis;cd $fis;sudo terraform plan;sudo terraform apply
 sleep 180
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg2  --min-size 1 --max-size 1 --vpc-zone-identifier $sub1
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg2  --min-size 1 --max-size 1 --vpc-zone-identifier $sub2
 aws autoscaling delete-launch-configuration --launch-configuration-name $lcfg1
 
 else
-  rm -rf $fis/terraform.*;cp userdata.sh $sec;cd $sec;sudo terraform plan;sudo terraform apply
+ # rm -rf $fis/terraform.*;cp userdata.sh $sec;cd $sec;sudo terraform plan;sudo terraform apply
  sleep 180
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg1  --min-size 1 --max-size 1 --vpc-zone-identifier $sub1
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg1  --min-size 1 --max-size 1 --vpc-zone-identifier $sub2
