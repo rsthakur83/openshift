93fc49f60891bfb8c84a2e094d5b1d6dbcc532ea
rsthakur83 <rsthakur83@yahoo.com>
2017-06-14 00:52:54 +0700

    Create scaling.sh

--- scaling.sh
+++ scaling.sh
@@ -17,16 +17,16 @@ if [ "$asg1" == "$lcfg1" ]
 then
 rm -rf $fis/terraform.*;cp userdata.sh $fis;cd $fis;sudo terraform plan;sudo terraform apply
 sleep 120
-sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg2  --min-size 1 --max-size 1 --vpc-zone-identifier $sub1
-sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg2  --min-size 1 --max-size 1 --vpc-zone-identifier $sub2
+sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg2  --min-size 2 --max-size 3 --vpc-zone-identifier $sub1
+sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg2  --min-size 2 --max-size 3 --vpc-zone-identifier $sub2
 sleep 120
 sudo aws autoscaling delete-launch-configuration --launch-configuration-name $lcfg1
 
 else
  rm -rf $fis/terraform.*;cp userdata.sh $sec;cd $sec;sudo terraform plan;sudo terraform apply
  sleep 120
-sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg1  --min-size 1 --max-size 1 --vpc-zone-identifier $sub1
-sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg1  --min-size 1 --max-size 1 --vpc-zone-identifier $sub2
+sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg1  --min-size 2 --max-size 3 --vpc-zone-identifier $sub1
+sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg1  --min-size 2 --max-size 3 --vpc-zone-identifier $sub2
 sleep 120
 sudo aws autoscaling delete-launch-configuration --launch-configuration-name $lcfg2
 
