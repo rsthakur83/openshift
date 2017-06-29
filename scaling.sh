5cc12502236b90d76b8a1c6e8b202ac9040e82ac
rsthakur83 <rsthakur83@yahoo.com>
2017-06-29 20:33:43 +0700

    Update scaling.sh

--- scaling.sh
+++ scaling.sh
@@ -28,7 +28,7 @@ sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine
 sleep 120
 sudo aws autoscaling delete-launch-configuration --launch-configuration-name $lcfg1
 ## Test ##
-#sleep 60
+sleep 60
 sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg2 --min-size 2 --max-size 2
 else
  rm -rf $fis/terraform.*;cp userdata.sh $sec;cd $sec;sudo terraform plan;sudo terraform apply
@@ -42,6 +42,6 @@ sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine
 sleep 120
 sudo aws autoscaling delete-launch-configuration --launch-configuration-name $lcfg2
 ## Test ##
-#sleep 60
+sleep 60
 sudo aws autoscaling update-auto-scaling-group --auto-scaling-group-name machine-factory-v1 --launch-configuration-name $lcfg1 --min-size 2 --max-size 2
 fi
