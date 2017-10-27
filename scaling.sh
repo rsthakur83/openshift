1e2fe4098cd949a0e9e7f12bb6777019ce6d1983
rsthakur83 <rsthakur83@yahoo.com>
2017-10-27 18:10:15 +0700

    Update scaling.sh

--- scaling.sh
+++ scaling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-vpcid="vpc-29bf7751"
+vpcid="vpc-6fc16717"
 
 
 asg1=`sudo aws autoscaling describe-launch-configurations --region us-east-1|grep LaunchConfigurationName|awk '{print $2}'|cut -c 2-19`
