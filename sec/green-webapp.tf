11a64d8f88926e63aa1b6e991639661714a4950f
rsthakur83 <rsthakur83@yahoo.com>
2018-05-04 11:06:12 +0200

    Update green-webapp.tf

--- sec/green-webapp.tf
+++ sec/green-webapp.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_launch_configuration" "machine-factory-v1" {
     name = "machine-factory-v1"
     image_id = "ami-b63769a1"
-     security_groups = ["sg-59968a10","sg-22e2fe6b"]
+     security_groups = ["sg-db889292","sg-e5948eac"]
     instance_type = "t2.micro"
     user_data       = "${file("userdata.sh")}"
     lifecycle              { create_before_destroy = true }
