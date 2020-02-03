b537b7983bc99cfe8aba355bf83dba65df44bc0b
rsthakur83 <rsthakur83@yahoo.com>
2020-02-03 20:26:38 +0100

    Update sg.tf

--- sec/sg.tf
+++ sec/sg.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2"
 }
 
 
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_launch_configuration" "machine-factory-v1" {
     name = "machine-factory-v1"
     image_id = "ami-b63769a1"
-    security_groups = ["sg-02bf8c10609829b6d","sg-06a991560907083f6"]
+    security_groups = ["sg-02b4c8d798290b687","sg-004c0c953c67a97e4"]
     instance_type = "t2.micro"
     user_data       = "${file("userdata.sh")}"
     lifecycle              { create_before_destroy = true }
