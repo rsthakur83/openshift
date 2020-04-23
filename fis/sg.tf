0c4f275f7b7f5ae9b03f287f7c25225ea05e5fef
rsthakur83 <rsthakur83@yahoo.com>
2020-04-23 22:35:46 +0200

    Update sg.tf

--- fis/sg.tf
+++ fis/sg.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_launch_configuration" "machine-factory-v2" {
     name = "machine-factory-v2"
     image_id = "ami-b63769a1"
-     security_groups = ["sg-0c4af813c971b5628","sg-019b63c323cf9271b"]
+    security_groups = ["sg-098faf39479fa0e14","sg-08093c7b958b1265f"]
     instance_type = "t2.micro"
     user_data       = "${file("userdata.sh")}"
     lifecycle              { create_before_destroy = true }
